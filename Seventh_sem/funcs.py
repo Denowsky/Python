004d4f000a7c463d5e410015293b2d707bf7315f
Denowsky <denowsky26@yahoo.com>
2023-09-20 16:56:04 +0300

    recoded

--- Seventh_sem/funcs.py
+++ Seventh_sem/funcs.py
@@ -2,66 +2,71 @@ import os
 import random as rnd
 from string import ascii_lowercase
 
-def create_file(name: str, lines=10):
-    with open(name, "w", encoding="utf-8") as file:
-        for _ in range(lines):
-            a = rnd.randint(-1000, 1000)
-            b = rnd.uniform(-1000, 1000)
-            file.write(f'{a} | {b}\n')
 
-def generate_name(size_min=4, size_max=7):
+def func_append(name: str, lines=10):
+    file = open(name, "w", encoding="utf-8")
+    for _ in range(lines):
+        a = rnd.randint(-1000, 1000)
+        b = rnd.uniform(-1000, 1000)
+        file.write(f'{a} | {b}\n')
+    file.close()
+
+
+def func_generate_name(size_min=4, size_max=7):
     vowels = set('euioa')
     consonants = set(ascii_lowercase).difference(vowels)
     len_name = rnd.randint(size_min, size_max)
     name = ''
     for i in range(len_name):
-        name += rnd.choice(list(vowels)) if i % 2 else rnd.choice(list(consonants))
+        name += rnd.choice(list(vowels)
+                           ) if i % 2 else rnd.choice(list(consonants))
     return name.title()
 
-def create_files(directory):
+
+def func_create_file(directory):
     count = int(input("Сколько файлов создать?: "))
     file_type = "." + input("Введите расширение файла: ")
     for i in range(count):
-        name = generate_name()
-        create_file(os.path.join(directory, name + file_type))
+        name = func_generate_name()
+        func_append(directory + name + file_type)
 
-def rename_files(directory, name_range=[3, 6]):
+
+def func_rename(directory, name_range=[3, 6]):
     os.chdir(directory)
-    files_list = [entry.name for entry in os.scandir(directory) if entry.is_file()]
-    if not files_list:
-        print(f'В директории "{directory}" нет файлов.')
-        return
-    print(f'В директории "{directory}" есть файлы: {files_list}')
-    answer = input('Желаете их переименовать? (Y/N): ')
-    if answer.lower() == "y":
-        file_type = "." + input("Введите расширение файла: ")
-        for index, file in enumerate(files_list, start=1):
-            name_parts = file.split(".")
-            ranged_name = name_parts[0][name_range[0]:name_range[1]]
-            number = "_" + str(index)
-            new_name = input(f'Введите новое имя для {name_parts[0]}: ')
-            os.rename(file, ranged_name + new_name + number + file_type)
-    elif answer.lower() == "n":
-        print("Возвращение в меню")
-    else:
-        print("Команда не распознана. Завершение работы")
+    files_list = os.listdir()
+    answer = input(
+        f'В директории "{directory}" есть файлы: {files_list}\nЖелаете их переименовать? Y/N: ')
+    match answer.lower():
+        case "y":
+            file_type = "." + input("Введите расширение файла: ")
+            for file in files_list:
+                name = file.split(".")
+                ranged_name = name[0][name_range[0]:name_range[1]]
+                number = "_" + str(files_list.index(file) + 1)
+                get_name = input(f'Введите новое имя для {name[0]}: ')
+                os.rename(file, ranged_name + get_name + number + file_type)
+        case "n":
+            print("Возвращение в меню")
+            return
+        case _:
+            print("Команда не распознана. Завершение работы")
+            return
 
-def main(directory):
-    while True:
-        print("Выберите пункт меню:")
-        print("1. Создать файлы.")
-        print("2. Переименовать файлы.")
-        print("3. Выход.")
-        choice = input("Введите номер пункта: ")
-        if choice == "1":
-            create_files(directory)
-        elif choice == "2":
-            rename_files(directory)
-        elif choice == "3":
-            print("Работа окончена, выход...")
-            break
-        else:
-            print("Команда не распознана")
 
-if __name__ == "__main__":
-    main("Seventh_sem/files/")
+def menu(directory):
+    status = True
+    while status == True:
+        menu_answer = int(input("\n1. Создать файл.\
+                                \n2. Посмотреть список файлов и переименовать их.\
+                                \n3. Выход\nВыберите пункт меню: "))
+        match menu_answer:
+            case 1:
+                func_create_file(directory)
+            case 2:
+                func_rename(directory)
+            case 3:
+                print("Работа окончена, выход...")
+                status = False
+            case _:
+                print("Команда не распознана")
+    return
---
+++ Seventh_sem/main.py
@@ -0,0 +1,5 @@
+from funcs import menu
+
+work_directory = 'Seventh_sem/files/'
+
+menu(work_directory)
